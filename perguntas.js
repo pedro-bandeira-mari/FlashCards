4a1cecd3900c52142cbdea935cdb137d5a474b27
pedro-bandeira-mari <pedrobmari@gmail.com>
2024-11-12 00:09:27 -0300

    Primeira versão do site

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é París.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz "Oi" em Inglês?',
+    'Se diz "Hi" (Pronúncia: "Rai").'
+)
